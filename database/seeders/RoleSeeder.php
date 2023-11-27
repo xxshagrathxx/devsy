3c6db4d7d7b62afc303c0a8935e97f61b7aaf51c
Mostafa <mostafa.m.a.w@gmail.com>
2023-11-27 02:35:13 +0200

    Initial commit

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::create(['name' => 'Admin']);
+        foreach (Permission::all() as $permission) {
+            $role->givePermissionTo($permission->name);
+        }
+
+        Role::create(['name' => 'Customer']);
+    }
+}
